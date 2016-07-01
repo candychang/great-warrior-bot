55e160727bef5f81b2d50e58460986945957794e
Michael Chang <chang313@purdue.edu>
2016-07-01 14:38:09 -0700

    refactoring views

--- line_bot/tests.py
+++ line_bot/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from line_bot.views import home_page
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 class homeTest(TestCase):
 	def test_home_url(self):
@@ -11,7 +12,6 @@ class homeTest(TestCase):
 	def test_home_correctect(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<p>Welcome</p>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
 		
--- line_bot/views.py
+++ line_bot/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><p>Welcome</p></html>')
+	return render(request, 'home.html')
