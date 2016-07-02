dc9b5d44ae3b713de51547de5d3945dfa6707f33
Candy Chang <candyichang@gmail.com>
2016-07-01 22:17:21 -0700

    split settings

---
+++ settings/dev.py
@@ -0,0 +1,5 @@
+from defaults import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['bot-staging.herokuapp.com']
---
+++ settings/production.py
@@ -0,0 +1,21 @@
+from defaults import *
+import dj_database_url
+
+DEBUG = TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['great-warrior-bot.herokuapp.com', 'bot-staging.herokuapp.com']
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
