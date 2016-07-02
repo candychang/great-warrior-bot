70eb65683360251b6e0f97b7701845e83e9d3712
Candy Chang <candyichang@gmail.com>
2016-07-02 10:30:40 -0700

    still working on setting up heroku

--- great_warrior_bot/settings.py
+++ great_warrior_bot/settings.py
@@ -21,13 +21,20 @@ DEBUG = TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['great-warrior-bot.herokuapp.com', 'bot-staging.herokuapp.com']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
