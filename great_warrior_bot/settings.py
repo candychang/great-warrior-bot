9958e5a523f1a9c04d117c0839601da02443c022
Candy Chang <candyichang@gmail.com>
2016-07-03 21:42:25 -0700

    still resolving static files prob

--- great_warrior_bot/settings.py
+++ great_warrior_bot/settings.py
@@ -33,15 +33,13 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
-)
-
+]
 
 
 
