e8a54e88954b8933f2584c65d1d9c551cfc82882
Candy Chang <candyichang@gmail.com>
2016-07-05 22:10:56 -0700

    seeing if 403 is because of csrf

--- line_bot/views.py
+++ line_bot/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from line_bot import line_api
 
 # Create your views here.
