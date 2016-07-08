25006f04d532228cbb688ec1b2e301ec1402adb9
Candy Chang <candyichang@gmail.com>
2016-07-07 18:44:04 -0700

    curl gives 400 but line gives 500

--- line_bot/views.py
+++ line_bot/views.py
@@ -14,11 +14,12 @@ def callback(request):
     if request.method == 'POST':
         
         signature = request.META['HTTP_X_LINE_CHANNELSIGNATURE']
+        render(request, 'callback.html', {'sig': signature})
 
-        if line_api.validate_signature(request.body, signature, settings.LINE_SECRET):
-            return HttpResponse('<html><p>RECEIVED</p></html>')
-        else:
-            return HttpResponseBadRequest()
+        # if line_api.validate_signature(request.body, signature, settings.LINE_SECRET):
+        #     return HttpResponse('<html><p>RECEIVED</p></html>')
+        # else:
+        #     return HttpResponseBadRequest()
 
 
     elif request.method == 'GET':
