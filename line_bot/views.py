f9d9f6d9c7260348937318ae2e8d130ce278d93b
Candy Chang <candyichang@gmail.com>
2016-07-07 19:15:06 -0700

    forget validation for now, checking request body

--- line_bot/views.py
+++ line_bot/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from line_bot import line_api
 from django.conf import settings
+import json
+
 
 # Create your views here.
 def home_page(request):
@@ -14,11 +16,13 @@ def callback(request):
     if request.method == 'POST':
         
         signature = request.META['HTTP_X_LINE_CHANNELSIGNATURE']
+        received_json_data = json.loads(request.body.decode("utf-8"))
+        return render(request, 'callback.html', {'sig': received_json_data})
 
-        if line_api.validate_signature(request.body, signature, settings.LINE_SECRET):
-            return render(request, 'callback.html', {'sig': signature})
-        else:
-            return HttpResponseBadRequest()
+        # if line_api.validate_signature(request.body, signature, settings.LINE_SECRET):
+        #     return render(request, 'callback.html', {'sig': signature})
+        # else:
+        #     return HttpResponseBadRequest()
 
 
     elif request.method == 'GET':
