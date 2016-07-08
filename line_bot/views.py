cd5aa0b860162265b81ec50aa0b355c113b0d3c3
Candy Chang <candyichang@gmail.com>
2016-07-07 19:49:58 -0700

    fixed syntax error

--- line_bot/views.py
+++ line_bot/views.py
@@ -19,7 +19,7 @@ def callback(request):
         signature = request.META['HTTP_X_LINE_CHANNELSIGNATURE']
         received_json_data = json.loads(request.body.decode("utf-8"))
         for message in received_json_data.result:
-            Message.objects.create('sender': message.content.from, 'content': message.content.text)
+            Message.objects.create(sender=message.content.from, content=message.content.text)
 
         return HttpResponse()
 
