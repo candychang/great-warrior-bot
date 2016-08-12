180953b395c1895cb754f74f2b264be01489254c
Candy Chang <candyichang@gmail.com>
2016-08-11 17:20:31 -0700

    removing duplicate code

--- line_bot/views.py
+++ line_bot/views.py
@@ -11,31 +11,6 @@ from line_bot.models import *
 def home_page(request):
 	return render(request, 'home.html')
 
-# @csrf_exempt
-# def callback(request):
-#     if request.method == 'POST':
-#         signature = request.META['HTTP_X_LINE_CHANNELSIGNATURE']
-#         if line_api.validate_signature(request.body, signature, settings.LINE_SECRET):
-#             received_json_data = json.loads(request.body.decode("utf-8"))
-#             sending_user = ""
-#             sent_text = ""
-#             for event in received_json_data["result"]:
-#                 c = r["content"]
-#                 sending_user = sending_user + c["from"]
-#                 sent_text= sent_text + c["text"]
-#                 m = Message(sender=sending_user, content=sent_text)
-#                 m.save()
-#             messages = Message.objects.all()
-#             index = len(messages)
-#             last_message = messages[index - 1]
-#             to_send = "HI! This is LineBot. You sent me this message: " + last_message.content
-#             sending_user = last_message.sender
-#             r = line_api.send_message(to_send, sending_user)
-#             return HttpResponse()
-#         else:
-#             return HttpResponseBadRequest()
-
-
 @csrf_exempt
 def callback(request):
 
